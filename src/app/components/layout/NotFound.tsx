6fe3066c429607dc0e6de130c7eb157c8c53592c
basit740 <alib4111@gmail.com>
2025-10-08 14:58:35 +0500

    updated 404 Not Found Page

--- src/app/components/layout/NotFound.tsx
+++ src/app/components/layout/NotFound.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import React from "react";
+import Button from "../ui/Button";
+import { useRouter } from "next/navigation";
+
 
 const NotFound = () => {
+  const router = useRouter();
+
   return (
-    <div className="h-full w-full border border-[#1D9ED9] p-8 text-center flex flex-col items-center justify-center bg-[#1D9ED90F]">
+    <div className="h-full w-full p-8 text-center flex flex-col items-center justify-center">
       <Image
         src="/images/NotFoundBg.png"
         alt="not_found_bg"
@@ -19,6 +24,14 @@ const NotFound = () => {
           “Sorry, we can’t seem to find the page you’re looking for. Let’s get
           you back home.”
         </p>
+      </div>
+      <div className="mt-6">
+        <Button
+          type="button"
+          title="Back to home"
+          variant="rounded-[167px] py-[7px] px-[22px] font-semibold text-white bg-[#1D9ED9] dark:bg-[#1D9ED9] dark:text-white border-2 dark:border border-white dark:border-white shadow-[0_4px_4px_rgba(0,0,0,0.25)] dark:shadow-none"
+          onClick={() => router.push("/")}
+        />
       </div>
     </div>
   );
